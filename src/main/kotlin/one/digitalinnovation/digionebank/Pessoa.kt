7ec5cc778677cb34698a12a882f175ba807b2d38
Kukukaia <kukukaia@protonmail.com>
2021-08-04 20:20:59 -0300

    update to talk about class members

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -5,11 +5,15 @@ class Pessoa {
 
     var cpf: String = "234.567.890-11"
     private set
+
+    constructor()
+
+    fun pessoaInfo() = "$nome e $cpf"
 }
 
 fun main() {
     val lilian = Pessoa()
 
-    println(lilian.nome)
-    println(lilian.cpf)
+
+    println(lilian.pessoaInfo())
 }
